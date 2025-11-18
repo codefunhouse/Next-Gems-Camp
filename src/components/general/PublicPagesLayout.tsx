b3047e5739a21af0b4518f39c24e235da849c948
ODINKS <nicholas.okeke87@gmail.com>
2025-11-18 12:20:19 +0100

    Chore(footer): rework footer to design

--- src/components/general/PublicPagesLayout.tsx
+++ src/components/general/PublicPagesLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 function PublicPagesLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="pt-[3.4rem] max-w-[90rem] mx-auto">
+    <div className="pt-[2.8rem] max-w-[90rem] mx-auto">
       <Navbar />
       <main className="min-h-screen flex flex-col">{children}</main>
       <Footer />
